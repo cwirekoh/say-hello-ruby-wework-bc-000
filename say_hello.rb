2a148fbecda7853023162b1e41ba9982c4f69542
Chantal Wirekoh <chantal.wirekoh@wework.com>
2018-02-09 00:27:02 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 name=""
-def say_hello (name)
+def say_hello (name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
 
-say_hello("Ruby Programmer")
+say_hello("Gabriela")
